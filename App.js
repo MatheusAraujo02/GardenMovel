79cae6b1eba09446cec7cc7d770c6ce9c87f1d1a
MatheusAraujo02 <127919173+MatheusAraujo02@users.noreply.github.com>
2024-08-07 19:23:10 -0300

    pouca att

--- App.js
+++ App.js
@@ -17,9 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'darkgreen',
+    backgroundColor: '#c0c0c0',
     // alignItems: 'center',
     // justifyContent: 'center',
-    padding: 10,
   },
 });
